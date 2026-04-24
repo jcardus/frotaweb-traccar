134d00152616899df029a44b20728f859212f188
Joaquim Cardeira <joaquim.cardeira@gmail.com>
2026-04-24 18:22:22 +0100

    Make onRequest async and add CORS support
    Refactor onRequest to be asynchronous and handle CORS headers.

--- functions/api/reports/[report].js
+++ functions/api/reports/[report].js
@@ -1,4 +1,4 @@
-export const onRequest = ({request, env}, cf) => {
+export const onRequest = async ({request, env}, cf) => {
     const url = new URL(request.url.replace('/traccar/', '/'))
     const oldest = new Date().setDate(new Date().getDate() - parseInt(env.DATABASE_RETENTION_WEEKS || '2') * 7)
     const forward = url.searchParams.get('from') && new Date(url.searchParams.get('from')) < new Date(oldest)
@@ -8,5 +8,10 @@ export const onRequest = ({request, env}, cf) => {
     url.protocol = forward ? 'https:' : 'http:'
     url.port = forward ? 443 : 80
     console.log(url)
-    return fetch(new Request(url, request), cf)
+    const response = await fetch(new Request(url, request), cf)
+    const origin = request.headers.get('Origin') || '*'
+    const headers = new Headers(response.headers)
+    headers.set('Access-Control-Allow-Origin', origin)
+    headers.set('Access-Control-Allow-Credentials', 'true')
+    return new Response(response.body, {status: response.status, statusText: response.statusText, headers})
 }
